debb0396127096d380de22f11b38a8faa4695025
ungden <tduong297@gmail.com>
2026-07-16 11:56:44 +0700

    chore(mobile): bump iOS release to 1.0.9

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "TruyenCity",
   slug: "truyencity",
-  version: "1.0.8",
+  version: "1.0.9",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "dark",
